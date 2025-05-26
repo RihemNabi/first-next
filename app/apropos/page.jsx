245ad017674d84903c55f355acbe546aef371f42
RihemNabi <ryhemnebi2004@gmail.com>
2025-05-26 16:53:01 +0100

    Ajout de la page A propos

---
+++ app/apropos/page.jsx
@@ -0,0 +1,11 @@
+export default function AproposPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>📄 Page À Propos</h1>
+      <p>Bienvenue sur la page À Propos de mon mini-projet Next.js ! ✨</p>
+      <p>
+        Je suis Rihem Nebi , une apprentie développeuse en pleine évolution 🚀
+      </p>
+    </div>
+  );
+}
