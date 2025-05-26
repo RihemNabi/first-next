34f659e25286360834854d2e255f7979226127b3
RihemNabi <ryhemnebi2004@gmail.com>
2025-05-26 14:02:51 +0100

    création projet Next.js et première page

---
+++ app/first/page.jsx
@@ -0,0 +1,9 @@
+export default function HelloPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🎉 Bienvenue , vous etes maintenant dans la page hello 🎉</h1>
+      <p>officiellement sur ma première page Next.js ✨</p>
+      <p>Lancement de mon mini-projet </p>
+    </div>
+  );
+}
