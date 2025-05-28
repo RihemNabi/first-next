caac14f402b39698551530249635b9607831fd59
RihemNabi <ryhemnebi2004@gmail.com>
2025-05-28 14:38:58 +0100

    Résolution des erreurs de configuration Tailwind CSS et vérification du style global

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 export default function Header() {
   return (
-    <header className="bg-pink-500 text-white p-4 text-center ">
-      <nav className="flex justify-center gap-6">
+    <div className="bg-pink-500  text-white p-4 text-center ">
+      <nav className="flex justify-around font-semibold">
         <Link href="/">Acceuil</Link>
         <Link href="/apropos">A propos</Link>
         <Link href="/contact">Contact</Link>
         <Link href="/services">Nos Services</Link>
       </nav>
-    </header>
+    </div>
   );
 }
