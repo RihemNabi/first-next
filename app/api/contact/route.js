72b2e5cb268d4b15091d5c30a11ac1552c35133d
RihemNabi <ryhemnebi2004@gmail.com>
2025-05-30 11:01:45 +0100

    succés de l'enregistrement des données du formulaire dans la base de donnée

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,9 +1,31 @@
-export async function POST(request) {
-  const body = await request.json();
-  console.log("Formulaire reçu :", body);
+// app/api/contact/route.js
+import { NextResponse } from "next/server";
+import clientPromise from "../../../lib/mongodb";
 
-  return new Response(JSON.stringify({ success: true }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+export async function POST(request) {
+  try {
+    const body = await request.json();
+    const client = await clientPromise;
+
+    const db = client.db("MiniProjetDB"); // !!!!! Nom de la base1
+    const collection = db.collection("messages");
+
+    await collection.insertOne({
+      name: body.name,
+      email: body.email,
+      message: body.message,
+      date: new Date(),
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: "Message enregistré !",
+    });
+  } catch (error) {
+    console.error("Erreur lors de l’insertion :", error);
+    return NextResponse.json(
+      { success: false, message: "Erreur serveur" },
+      { status: 500 }
+    );
+  }
 }
