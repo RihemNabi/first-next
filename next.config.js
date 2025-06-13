de53b7d38dcd99817c034b46a24f4d620e5f000d
RihemNabi <ryhemnebi2004@gmail.com>
2025-06-13 06:39:32 +0100

    supp

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-import { i18n } from "./next-i18next.config.js";
-
 const nextConfig = {
   experimental: {},
   serverExternalPackages: ["@next-auth/mongodb-adapter"],
-  i18n,
 };
 
 export default nextConfig;
